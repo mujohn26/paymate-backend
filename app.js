859302603f8f935dfc642ee0b1bde6bfb0b2eb05
kabuga2015@1 <boazkhalifa@gmail.com>
2023-09-07 07:16:07 -0700

    chore: sequalizer setup

---
+++ app.js
@@ -0,0 +1,13 @@
+import express from 'express';
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Welcome to paymate application!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
