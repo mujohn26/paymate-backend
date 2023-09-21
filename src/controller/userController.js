34b9bac23257ecbf5d1f9bacbe969bc17420547b
kabuga2015@1 <boazkhalifa@gmail.com>
2023-09-21 16:22:47 +0200

    login Endpoint

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,31 +1,28 @@
 import UserServices from "../services/user.services";
+import EncryptPassword from "../helpers/encrypt.helper";
 import GenerateOTP from "../helpers/otp.helper";
+import comparePassword from "../helpers/decryptor.helper";
+import TokenHelper from "../helpers/token.helper";
 class UserController {
 
   static async verifyOTP(req, res) {
     try {
-      const { otp } = req.body; // Assuming the OTP entered by the user is sent in the request body
-      const { phone } = req.params; // Assuming the phone number is part of the request parameters
-
-      // Retrieve the user by phone number
+      const { otp } = req.body;
+      const { phone } = req.params;
       const user = await UserServices.getUserByphone(phone);
-
       if (!user) {
         res.status(404).json({
           error: `User with phone ${phone} not found`,
         });
         return;
       }
-
-      const otpCreatedAt = user.createdAt; // Assuming you have a field to store the OTP creation timestamp
-
+      const otpCreatedAt = user.updateAt;
       if (GenerateOTP.isOTPExpired(otpCreatedAt)) {
         res.status(400).json({
           error: `The OTP has expired. Please request a new OTP.`,
         });
         return;
       }
-      // Check if the entered OTP matches the verificationCode
       if (otp === user.verificationCode.toString()) {
         user.isVerified = true;
         await user.save();
@@ -44,13 +41,41 @@ class UserController {
       });
     }
   }
+  // resend otp 
+  static async resendOTP(req, res) {
+    try {
+      const { phone } = req.params; 
+      const user = await UserServices.getUserByphone(phone);
+
+      if (!user) {
+        res.status(404).json({
+          error: `User with phone ${phone} not found`,
+        });
+        return;
+      }
+
+      const newOTP = Math.floor(1000 + Math.random() * 9000);
+      user.verificationCode = newOTP;
+
+      await user.save();
+      GenerateOTP.sendOTP(user.phone, newOTP);
+      res.status(200).json({
+        message: `New OTP sent successfully to user ${user.names}`,
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({
+        status: "Internal server error",
+      });
+    }
+  }
   static async registerUser(req, res) {
     try {
       const newUser = UserController.getRequestBodyData(req);
-      const existingUser = await UserServices.getUserByphone(newUser.phone); // Assuming UserServices.getUserByPhone is asynchronous
+      const existingUser = await UserServices.getUserByphone(newUser.phone);
 
       if (!existingUser) {
-        // Generate a random 4-digit OTP
+
         const otp = Math.floor(1000 + Math.random() * 9000);
         newUser.verificationCode = otp;
 
@@ -58,7 +83,7 @@ class UserController {
 
         if (result) {
 
-          // Send the OTP to the user's phone
+
           GenerateOTP.sendOTP(newUser.phone, otp);
 
           res.status(201).json({
@@ -83,12 +108,55 @@ class UserController {
     }
   }
 
+  // user login 
+  static async userLogin(req, res) {
+    try {
+      const loginInfo = {
+        phone: req.body.phone,
+        password: req.body.password,
+      };
+      console.log("=-=--=-=--=", loginInfo);
+
+      const existUser = await UserServices.getUserByphone(
+        loginInfo.phone
+      );
+      if (existUser !== null) {
+        let decryptPasswordAndCompare = comparePassword(
+          loginInfo.password,
+          existUser.dataValues.password
+        );
+        if (decryptPasswordAndCompare) {
+          const payload = {
+            names: existUser.dataValues.names,
+            phone: existUser.dataValues.phone,
+          };
+          const token = await TokenHelper.generateToken(payload);
+          res.status(200).json({
+            message: `User ${existUser.names} logged in successfully`,
+            token,
+          });
+        } else {
+          res.status(400).json({
+            error: `Invalid telephone number or password. Please try again`,
+          });
+        }
+      } else {
+        res.status(404).json({
+          error: `User with this telephone does not exist`,
+        });
+      }
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({
+        status: "Internal server error",
+      });
+    }
+  }
   static getRequestBodyData(req) {
     return {
       names: req.body.names,
-      password: req.body.password,
+      password: EncryptPassword(req.body.password),
       phone: req.body.phone,
-      // verificationCode: req.doby.verificationCode
     };
   }
   static async getAllUsers(req, res) {
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -4,8 +4,10 @@ const router = express.Router();
 
 
 router.post("/create",userController.default.registerUser);
+router.post("/login",userController.default.userLogin);
 router.delete("/delete/:id", userController.default.deleteUser);
-router.get("/",userController.default.getAllUsers)
-router.patch("/verify/:phone",userController.default.verifyOTP)
+router.get("/",userController.default.getAllUsers);
+router.patch("/verify/:phone",userController.default.verifyOTP);
+router.patch("/resend/:phone",userController.default.resendOTP);
 
 module.exports = router;
